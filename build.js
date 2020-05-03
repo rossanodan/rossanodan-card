2f6adfbf24686c975bf53fbfcaea34e16b743c34
rossanodan <rossanodan@gmail.com>
2020-05-03 14:33:37 +0100

    Updated details

--- build.js
+++ build.js
@@ -17,12 +17,13 @@ const options = {
 const data = {
   name: chalk.white("               Rossano D'Angelo"),
   handle: chalk.white('rossanodan'),
-  work: chalk.white('Front End Developer at IBM'),
+  work: chalk.white('Front End Developer at Wilmington plc'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('rossanodan'),
   npm: chalk.gray('https://npmjs.com/') + chalk.red('~rossanodan'),
   github: chalk.gray('https://github.com/') + chalk.green('rossanodan'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('rossanodan'),
   web: chalk.cyan('https://dev.to/rossanodan'),
+  blog: chalk.cyan('https://rossanodan.me/'),
   npx: chalk.red('npx') + ' ' + chalk.white('rossanodan'),
   labelWork: chalk.white.bold('       Work:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
@@ -30,6 +31,7 @@ const data = {
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
   labelWeb: chalk.white.bold('        Web:'),
+  labelBlog: chalk.white.bold('       Blog:'),
   labelCard: chalk.white.bold('       Card:')
 }
 
@@ -42,6 +44,7 @@ const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
+const blogging = `${data.labelBlog}  ${data.blog}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
@@ -52,7 +55,8 @@ const output = heading + // data.name + data.handle
                npming + newline + // data.labelnpm + data.npm
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
+               webing + newline + // data.labelWeb + data.web
+               blogging + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
